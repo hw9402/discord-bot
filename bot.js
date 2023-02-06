d2967301994238d7f4f4793f0cbc5e28e67e50b7
마현우 <mhw35689402@gmail.com>
2023-02-06 18:09:09 +0900

    UPDATE :: 커맨드 핸들러를 이용한 ping 명령어 제작

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const Discord = require('discord.js');
+const { Client, GatewayIntentBits } = Discord;
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -7,20 +8,32 @@ const client = new Client({
 		GatewayIntentBits.GuildMembers,
 	],
 });
-const dotenv = require('dotenv').config();
+
+const {prefix, token} = require('./config.json');
+const fs = require('node:fs');
+
+client.commands = new Discord.Collection();
+const commandsFile = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandsFile) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
     console.log(`${client.user.tag} 봇에 로그인 했습니다`);
 });
 
 client.on('messageCreate', (msg) => {
-    try {
-        if(msg.content === 'ping') {
-            msg.reply('pong!');
-        }
-    } catch(error) {
-        console.error(error);
-    }
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
+	const args = msg.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift();
+	const command = client.commands.get(commandName);
+	try {
+		command.execute(msg, args);
+	} catch(error) {
+		console.error(error);
+	}
 });
 
-client.login(process.env.TOKEN);
+client.login(token);
